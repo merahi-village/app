d590a03be7561a15f5ec8f8a64d649e1aeda2cb6
Gitesh Sharma <38495884+gitesh-sharma@users.noreply.github.com>
2026-09-23 23:02:12 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,3 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = { appId:'com.merahi.green', appName:'Merahi Green', webDir:'dist', bundledWebRuntime:false, server:{androidScheme:'https'} };
+
+const config: CapacitorConfig = {
+  appId: 'com.merahi.mone',
+  appName: 'mOne',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
 export default config;
